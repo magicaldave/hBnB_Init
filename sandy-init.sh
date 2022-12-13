4a0101330b02f4c788d2e74559e4804684bb4fc6
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-12-13 16:26:59 -0600

    Add: Sandy-init and sandy-run scripts.

---
+++ sandy-init.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Provide your github username, then the file to run as arguments.
+# Update, install dependencies, and autoremove
+if [ ! -d "$HOME/holbertonschool-AirBnB_clone_v4/" ]; then
+    if [ ! "$1" ]; then
+	REPO=https://github.com/Coleraines13/holbertonschool-AirBnB_clone_v4.git
+    else
+	REPO=https://github.com/$1/holbertonschool-AirBnB_clone_v4.git
+    fi
+else
+    rm -rf ~/holbertonschool-AirBnB_clone_v4
+fi
+if [ ! -e "$HOME/sandy-run.sh" ]; then
+    wget
+fi
+apt-get update && apt-get upgrade
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+apt-get install -y nodejs mysql-server python3-dev libmysqlclient-dev zlib1g-dev python3-lxml && apt autoremove
+pip3 install mysqlclient SQLAlchemy Flask flask_cors flasgger pathlib2 jsonschema==3.0.1
+npm install semistandard request --global
+export NODE_PATH=/usr/lib/node_modules
+
+# Go home
+cd
+# Start mySQL
+sudo service mysql start
+# Pull the repo
+git clone $REPO
+# Move into the repository
+cd holbertonschool-AirBnB_clone_v4/
+# Set up the database
+cat web_flask/100-dump.sql | mysql
+# Dis is da sauce right here bois
+if [ "$2" ]; then
+    PROJ=$2
+else
+    PROJ=0
+fi
+~/sandy-run.sh $PROJ
---
+++ sandy-run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Provide the file to run as arguments, 0 by default.
+cd ~/holbertonschool-AirBnB_clone_v4/
+if [ "$1" ]; then
+    PROJ=$1
+else
+    PROJ=0
+fi
+
+if ([ ! -z $(pgrep -f "python3 -m api.v1.app") ]); then
+    echo "Api is already up!"
+    HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db python3 -m web_dynamic.$PROJ-hbnb
+else
+    HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db HBNB_API_PORT=5001 python3 -m api.v1.app & HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db python3 -m web_dynamic.$PROJ-hbnb
+fi
